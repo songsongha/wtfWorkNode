beee8d8b6a0d24977cfaa007da0d198ad70eeaca
Song <songsongha@gmail.com>
2024-07-15 13:00:15 -0600

    Process scraped data to desired format

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const url = 'https://www.jobbank.gc.ca/marketreport/summary-occupation/21732/ca';
+const url = 'https://www.jobbank.gc.ca/marketreport/summary-occupation/2133/ca';
 
-console.log('does this run?')
 rp(url)
   .then(function(html){
-    //success!
-    console.log('does this succeedd?')
-    console.log({html})
     const $ = cheerio.load(html)
-    console.log($('.heading-info').first().text());
-    // console.log($('.section-value:contains("$")').text());
-    // console.log($('.section-value:contains("jobs")').text());
-    console.log($('.section-value').text());
+    const jobTitle = $('.heading-info').text().trim().replace(' in Canada', '');
+    const requirements = $('#j_id_2n_2_25 .section-value').text().trim(); // sometimes comes up as management
+    const wageString = $('#j_id_2n_2_2c .section-value').text().trim();
+    const wageUnit = wageString.replace(/[$.,/0-9 ]/g,'')
+    const wage = Number(wageString.replace(/[$,/a-z ]/g,''))
+    let wageHour = 0;
+    let wageYear = 0;
+    if (wageUnit === 'hour'){
+        wageHour = wage;
+        wageYear = wage * 2000;
+    } else if (wageUnit === 'year'){
+        wageYear = wage;
+        wageHour = wage / 2000;
+    } 
+
+    console.log({wageUnit})
+    console.log({wage})
+    console.log({wageHour})
+    console.log({wageYear})
+    
+    const numPosting = Number($('#j_id_2n_2_44 .section-value').text().trim().split(' ')[0]);
+    console.log({jobTitle})
+    console.log({requirements})
+    console.log({wage})
+    console.log({numPosting})
   })
   .catch(function(err){
     console.log('is there an error?', err)
