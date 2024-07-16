b21d3a2dca3edee4b094f73fba7ae6e7d93e54cc
Song <songsongha@gmail.com>
2024-07-16 15:41:21 -0600

    Add writing scraped data to databased

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const url = 'https://www.jobbank.gc.ca/marketreport/summary-occupation/2133/ca';
+const sqlite3 = require('sqlite3').verbose()
 
-rp(url)
-  .then(function(html){
-    const $ = cheerio.load(html)
-    const jobTitle = $('.heading-info').text().trim().replace(' in Canada', '');
-    const requirements = $('#j_id_2n_2_25 .section-value').text().trim(); // sometimes comes up as management
-    const wageString = $('#j_id_2n_2_2c .section-value').text().trim();
-    const wageUnit = wageString.replace(/[$.,/0-9 ]/g,'')
-    const wage = Number(wageString.replace(/[$,/a-z ]/g,''))
-    let wageHour = 0;
-    let wageYear = 0;
-    if (wageUnit === 'hour'){
-        wageHour = wage;
-        wageYear = wage * 2000;
-    } else if (wageUnit === 'year'){
-        wageYear = wage;
-        wageHour = wage / 2000;
-    } 
+const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err)=>{
+    if (err) return console.error(err.message)
+        console.log('connection successful')
+})
 
-    console.log({wageUnit})
-    console.log({wage})
-    console.log({wageHour})
-    console.log({wageYear})
+// // Create table
+// db.run(
+//     'CREATE TABLE jobs(url, job_title, wage_hour, wage_year, requirements, num_postings)'
+// )
+
+const insertSQL = 'INSERT INTO jobs (url, job_title, wage_hour, wage_year, requirements, num_postings) VALUES (?,?,?,?,?,?)'
+
+for (let i = 2012; i < 2017; i++){
+    const url = `https://www.jobbank.gc.ca/marketreport/summary-occupation/${i}/ca`;
+
+    rp(url)
+        .then(html => {
+        const $ = cheerio.load(html)
+        const jobTitle = $('.heading-info').text().trim().replace(' in Canada', '');
+        const requirements = $('#j_id_2n_2_25 .section-value').text().trim(); // sometimes comes up as management
+        const wageString = $('#j_id_2n_2_2c .section-value').text().trim();
+        const wageUnit = wageString.replace(/[$.,/0-9 ]/g,'')
+        const wage = Number(wageString.replace(/[$,/a-z ]/g,''))
+        let wageHour = 0;
+        let wageYear = 0;
+        if (wageUnit === 'hour'){
+            wageHour = wage;
+            wageYear = wage * 2000;
+        } else if (wageUnit === 'year'){
+            wageYear = wage;
+            wageHour = wage / 2000;
+        } 
+        
+        const numPosting = Number($('#j_id_2n_2_44 .section-value').text().trim().replace(/[$,/a-z ]/g,''));
+        console.log({url})
+        console.log({jobTitle})
+        console.log({requirements})
+        console.log({wageString})
+        console.log({numPosting})
+
+        if (jobTitle) db.run(insertSQL, [url, jobTitle, wageHour, wageYear, requirements, numPosting])
     
-    const numPosting = Number($('#j_id_2n_2_44 .section-value').text().trim().split(' ')[0]);
-    console.log({jobTitle})
-    console.log({requirements})
-    console.log({wage})
-    console.log({numPosting})
-  })
-  .catch(function(err){
-    console.log('is there an error?', err)
-    //handle error
-  });
+        })
+        .catch((err) => {
+        console.log({err})
+        //handle error
+        });
+
+}
+
+
+
+//   const viewSQL= 'SELECT * FROM jobs'
+
+//   db.all(viewSQL,[], (err, rows)=>{
+//     if (err) console.error(err.message)
+//     rows.forEach((row)=>{
+//         console.log(row)
+//     })
+// })
+// db.close((err) => {
+//     if (err) return console.error(err.message)
+//         console.log('db close successful')
+// })
